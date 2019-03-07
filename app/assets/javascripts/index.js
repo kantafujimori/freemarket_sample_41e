859f34a453ac7265008624b52ce22db69f05817c
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-03-07 21:29:16 +0900

    Add slick
    ・インジゲーターのデザインが実装できない。
    ・arrowsのデザインが実装できない。
    ・prevが機能しない。

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.slick').slick({
+    autoplay:true,
+    autoplaySpeed: 3000,
+    // // arrows: false,
+    dots: true,
+    prevArrow:'<div class="slide-prev">PREV</div>',
+    nextArrow:'<div class="slide-next">NEXT</div>',
+  });
+  // $('.slick-next').on('click', function () {
+  //       slick.slickNext();
+  //   });
+  //   $('.slick-prev').on('click', function () {
+  //       slick.slickPrev();
+  //   });
+});
