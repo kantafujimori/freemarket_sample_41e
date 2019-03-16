102ee4bf9ac124f777c0b4835c8ad82e7e5c15b6
yukihayashi <mrchildreny@gmail.com>
2019-03-16 20:38:44 +0900

    modify products_controller_spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,11 +5,14 @@ describe ProductsController do
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
+      end
+    it "リクエストは200となること" do
+      expect(response.status).to eq 200
     end
     it "populates an array of products ordered by created_at ASC" do
       products = create_list(:product, 3)
       get :index
-      expect(assigns(:products)).to match(products)
+      expect(assigns(:products)).to eq(products)
     end
   end
 end
