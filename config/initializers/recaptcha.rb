c62c16464db4a32e9c60f44ac7a42b2888534fff
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-03-17 16:54:18 +0900

    Add rechapcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key = ENV['6LdzBJgUAAAAAG4ORje-k-7ykQv2k-C1sQILBH1a']
-  config.private_key = ENV['6LdzBJgUAAAAACkv58EdkLrHBJWvJFyHqr0P75bz']
+  config.site_key = '6LdzBJgUAAAAAG4ORje-k-7ykQv2k-C1sQILBH1a'
+  config.secret_key = '6LdzBJgUAAAAACkv58EdkLrHBJWvJFyHqr0P75bz'
 end
