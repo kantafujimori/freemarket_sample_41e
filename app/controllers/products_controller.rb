95678f80cd044f10bde3ff5e11d04178390818b0
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-30 03:58:54 +0900

    items-in-user-profile作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order("created_at ASC").limit(4)
   end
+
+  def show
+    @products = Product.order("created_at ASC").limit(3)
+  end
+
   def new
   end
 end
