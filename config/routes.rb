4f03cb12a12242b6ee66375c42b5a34a9ac4757b
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-03-31 01:24:55 +0900

    Add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "/users/signup" => "users#signup"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end
---
+++ db/migrate/20190326104623_add_nickname_to_users.rb
@@ -0,0 +1,8 @@
+class AddNicknameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :birth_year, :integer, null: false
+    add_column :users, :birth_month, :integer, null: false
+    add_column :users, :birth_day, :integer, null: false
+  end
+end
