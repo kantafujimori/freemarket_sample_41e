28293805895351aa0525b9366404411e9b9f14cc
yukihayashi <mrchildreny@gmail.com>
2019-04-01 16:06:43 +0900

    creating sell-form

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -4,6 +4,21 @@ class SellsController < ApplicationController
   end
   def new
     #https://www.mercari.com/jp/sell/の部分
+    @sell = Product.new
+  end
 
+  def create
+    @sell = Product.new(sell_params)
+    if @sell.save
+      redirect_to sell_path(@sell)
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def sell_params
+    params.require(:product).permit(:name, :info, :price, :status)
   end
 end
