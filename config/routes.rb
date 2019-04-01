91a7a8efd48f1be7cd5d825a26ca88cd259dc1de
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-04-01 23:19:07 +0900

    Add templeate

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'products#index'
   devise_for :users
-  resources :users, only: [:show]
-  get "/users/signup" => "users#signup"
+  resources :users do
+    collection do
+      get :signup
+    end
+  end
   resources :products do
     get :buy_confirm, on: :collection, as: :buy
   end
