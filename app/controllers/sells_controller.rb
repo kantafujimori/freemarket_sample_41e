2cb1d1089d51825f9cbb99def77ea4bc8adbf1c6
yukihayashi <mrchildreny@gmail.com>
2019-04-04 14:07:22 +0900

    modify def shipping_method
    fields_forを追加して @sell = Product.newに対して個別で:shipping_method_idを追加する様にした。

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -18,6 +18,7 @@ class SellsController < ApplicationController
   end
 
   def shipping_method
+    @sell = Product.new
     render partial: 'sells/shipping_method', locals: { delivery_fee_owner_id: params[:delivery_fee_owner_id] }
   end
 
@@ -25,8 +26,5 @@ class SellsController < ApplicationController
 
   def sell_params
     params.require(:product).permit(:delivery_fee_owner_id, :shipping_method_id, :name, :info, :price, :status)
-  end
-  def shipping_method_params
-    params.permit(:shipping_method)
   end
 end
