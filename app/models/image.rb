5ec58786bb9d48ec4ff8387cd9306db6cfab0bed
yukihayashi <mrchildreny@gmail.com>
2019-04-13 20:13:28 +0900

    add validates image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
   mount_uploaders :image, ImagesUploader
   belongs_to :product, optional: true
-  #validates :product_id, presence: true
+  validates :image, presence: true
+  validates :product_id, presence: true
 end
