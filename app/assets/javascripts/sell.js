20adbd3196b801dad52432afe0080e4772b73427
yukihayashi <mrchildreny@gmail.com>
2019-04-15 17:13:08 +0900

    marged category.js with sell.js

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,4 +1,33 @@
 $(document).on('turbolinks:load', function() {
+  //カテゴリー
+  $('#category-top').on('change',function () {
+    var value = $('#category-top option:selected').val();
+    console.log(value);
+    if (value.length){
+      $.ajax({
+        url: '/sells/category_middle',
+        type: "get",
+        data: {
+          parent_id: value
+        }
+      });
+    }
+  });
+  $('#category-middle').on('change',function () {
+    var top = $('#category-top option:selected').val();
+    var value = $('#category-middle option:selected').val();
+    $.ajax({
+      url: '/sells/category_bottom',
+      type: "get",
+      data: {
+        top_id: top,
+        parent_id: value,
+      }
+    })
+  });
+  $('#category-bottom').on('change',function () {
+    html = ``
+  });
     //配送方法
     $('#delivery_fee_owner').on("change",function() {
         var noselect = $('#delivery_fee_owner option:selected').val();
@@ -101,7 +130,5 @@ $(document).on('turbolinks:load', function() {
           }
         };
         reader.readAsDataURL(file);
-      //e.target.value = '';
-      //console.log(e.target.value);
     });
   });
