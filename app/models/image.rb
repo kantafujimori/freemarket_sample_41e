0b6b2e1e721b616c284a347598513445be508bad
yukihayashi <mrchildreny@gmail.com>
2019-04-16 01:17:20 +0900

    remove validates image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,4 @@
 class Image < ApplicationRecord
   mount_uploaders :image, ImagesUploader
   belongs_to :product, optional: true
-  validates :image, presence: true
-  validates :product_id, presence: true
 end
