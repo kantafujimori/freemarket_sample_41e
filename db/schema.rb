04a122c1c19828fcacf2019f50ae5e63201d3f29
yukihayashi <mrchildreny@gmail.com>
2019-04-15 23:34:40 +0900

    create size tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_074510) do
+ActiveRecord::Schema.define(version: 2019_04_15_143033) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 2019_04_15_074510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "delivery_fee_owner_id"
+  end
+
+  create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "size", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["size"], name: "index_sizes_on_size"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
