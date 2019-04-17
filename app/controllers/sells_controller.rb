b9e97d4a237b2c1b9562f9fa3f6a3215dbf07d5d
Shirakawa-Satoshi <kyabetsu3104@gmail.com>
2019-04-18 07:09:08 +0900

    modify sell.js for append product images
    # Conflicts:
    #	app/views/products/_item.html.haml

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -5,13 +5,13 @@ class SellsController < ApplicationController
     @sell = Product.new
     @shipping_method = ShippingMethod.new
     @categories = Category.eager_load(children: :children).where(parent_id: nil)
-
+    @image = Image.new
   end
 
   def create
     @sell = Product.new(sell_params)
 
-    if @sell.save
+    if @sell.save & save_images(@sell, image_params)
       redirect_to sell_path(@sell)
     else
       redirect_to root_path
@@ -33,6 +33,22 @@ class SellsController < ApplicationController
 
   def sell_params
     params.require(:product).permit(:delivery_fee_owner_id, :shipping_method_id,:delivery_date_id, :name, :info, :price, :status,
-    :size_id, :category_id, :shipping_from, :brand, images_attributes: {image: []})
+    :size_id, :category_id, :shipping_from, :brand)
+  end
+
+  def image_params
+    params.require(:images).require(:image).permit(params[:images][:image].keys) if params[:images].present?
+  end
+
+  def save_images(item, images)
+    if images.present?
+      return false if item.id.blank?
+      images.values.each do |name|
+        @image = item.images.create(image: name)
+      end
+    else
+      @image_error = "画像がありません"
+      return false
+    end
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,6 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 class Image < ApplicationRecord
-  mount_uploaders :image, ImagesUploader
+  mount_uploader :image, ImagesUploader
   belongs_to :product, optional: true
 end
