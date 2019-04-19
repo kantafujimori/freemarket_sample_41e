c9f95289f4a1078005c65ab025a6088f53887be5
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-04-19 19:13:10 +0900

    delete comment out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,4 @@ class UsersController < ApplicationController
   def show
     @products = Product.order("created_at ASC")
   end
-
-  # def create
-  #   Address.new(user_params)
-  # end
-
-
-  # private
-
-  # def user_params
-  #   params.require(:addres).permit(:first_name, :last_name, :first_name_phon, :last_name_phon)
-  # end
 end
