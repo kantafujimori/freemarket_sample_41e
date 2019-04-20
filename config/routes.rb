79f8465ad8c23c80b300d1211f3e1555e3b369fb
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2019-04-20 12:42:21 +0900

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
   devise_for :users
   resources :users do
     collection do
       get :signup
     end
   end
+  root 'products#index'
   resources :products do
     get 'shipping_method', on: :collection
     get 'category_middle', on: :collection
